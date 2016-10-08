ff0871cad71d60861f3f235f5fa9850fe28b5a62
AlessandroSona <alessandrosona@outlook.com>
2016-10-08 17:06:52 +0200

    Updated functions

--- ROD.ino
+++ ROD.ino
@@ -1,3 +1,13 @@
+#define CW 0
+#define CCW 1
+
+
+int pwmpin[2] = {5, 6}; // PWM's input
+int inApin[2] = {7, 4}; // INA: Clockwise
+int inBpin[2] = {8, 9}; // INB: Counterlockwise
+
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -20,13 +30,15 @@ void setup() {
   void loop() {
     drive();//continuously drive motor
   }
-  void drive(int x, int y, int velocity)//sends control signal to motor shield
+  void drive(int x, int y)//sends control signal to motor shield
   {
     int speed0;//speed left motor
     int speed1;//speed right motor
-    int direction0;//direction left motor
-    int direction1;//direction right motor
-    calcultateSpeedDirection (x, y, velocity);//calculate parameters
+    int direction0 = CW; //direction left motor
+    int direction1 = CW; //direction right motor
+    calcultateSpeedDirection (x, y);//calculate parameters
+    speed0 = map(velocityLeft, 0, controllerMax, 0, 255);
+    speed1 = map(velocityRight, 0, controllerMax, 0, 255);
     adjustMotor(0, direction0, speed0);//run left motor
     adjustMotor (1, direction1, speed1);//run right motor
   }
@@ -34,8 +46,16 @@ void setup() {
   {
 
   }
-  void calcultateSpeedDirection (x, y, velocity)//converts input in controller-format to suitable combination of track movements
+  void calcultateSpeedDirection (x, y)//converts input in controller-format to suitable combination of track movements
   {
-
+    int velocityLeft = (x / 2) + y;//compute velociy left track
+    if (velocityLeft < 0)// if needed change left direction
+    {
+      directionLeft = CCW;
+    }
+    int velocityRight = ((-x) / 2) + y;//see Left
+    if (velocityRight < 0)
+    {
+      directionRight = CCW;//see Left
+    }
   }
-
